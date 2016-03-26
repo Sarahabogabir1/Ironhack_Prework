52c66bcf9990856cab03ccf1219d5b9ba3563d12
Sarah Abogabir <abogabirsl@vcu.edu>
2016-03-26 13:39:05 -0400

    Delete cipher1.rb

--- 4.Cypher/cipher1.rb
+++
@@ -1,6 +0,0 @@
-def solve_cipher(word, shift)
-  deciphered_word = ''
-  word.each_char { |c| deciphered_word << (c.ord + shift).chr }
-  return deciphered_word
-end
-print solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
