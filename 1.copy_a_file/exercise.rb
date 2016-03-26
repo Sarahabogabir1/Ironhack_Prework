97cf8f97739b878afe059fe7b8b75d069ee696d6
Sarah Abogabir <abogabirsl@vcu.edu>
2016-03-26 13:37:50 -0400

    Delete exercise.rb

--- 1.copy_a_file/exercise.rb
+++
@@ -1,9 +0,0 @@
-puts "What is the source file?"
-source_file = gets.chomp
-
-puts "What would you like to name your new destination file?"
-destination_file = gets.chomp
-
-puts "You chose #{destination_file}"
-source_file_contents = IO.read(source_file)
-IO.write(destination_file, source_file_contents)  
